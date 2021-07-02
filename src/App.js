dc57f80991c67b6029077f9655aa7dd5ef074dc7
marcelo.faria <faria.marcelofilho@gmail.com>
2021-07-01 22:56:33 -0300

    fix .map error

--- src/App.js
+++ src/App.js
@@ -1,46 +1,47 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import apiTheMovieDb from './services/apiTheMovieDb';
 
-const initialData = {
-  image: '',
-  title:  '',
-  synopsis: '',
-  releaseDate: '',
-}
-
 function App() {
 
-  const [movies, setMovies] = useState(initialData);
+  const [movies, setMovies] = useState([]);
+  const defaultPath = 'http://image.tmdb.org/t/p/w185/';
 
   async function getDataFromApi() {
 
     const response = await apiTheMovieDb.get(`/upcoming?api_key=fe65f8e840e15d06c5c00bf13084da74&language=pt-BR&page=1`);
 
     console.log(response.data);
-    if (response.data.length) {
-      setMovies(initialData);
+    if (response.data) {
+      setMovies(response.data.results);
     }
 
+    console.log(movies)
+
   }
+
+  useEffect(() => {
+    try {
+      getDataFromApi();
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          testetesteteste
-        </p>
-        { movies.map((movie, key) => (
-          <span>{movie.results.title}</span>
-        )) }
-        <button
-          type="button"
-          onClick={(e) => getDataFromApi()}
-          className="btn btn-success btn-100"
-          >
-          Conhecer Filmes
-        </button>
+      <div class="row">
+        {movies.map(item => (
+          <div class="col-lg-3">
+            <div class="image">
+              <img src={defaultPath + item.poster_path}/>
+            </div>
+            <span>{item.title}</span>
+          </div>
+        ))}
+      </div>
       </header>
     </div>
   );
