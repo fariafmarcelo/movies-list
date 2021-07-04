312e72e4399824eaaa9655ffab3e07e38834bca1
marcelo.faria <faria.marcelofilho@gmail.com>
2021-07-04 18:50:26 -0300

    visual changes and start create routes components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+
+import React, { useState, useEffect, Component } from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+
 import 'bootstrap/dist/css/bootstrap.css';
+
 import apiTheMovieDb from './services/apiTheMovieDb';
+
+import FullMoviePage from './components/FullMoviePage';
 
 function App() {
 
   const [movies, setMovies] = useState([]);
-  const defaultPath = 'http://image.tmdb.org/t/p/w185/';
+  const defaultPath = 'https://image.tmdb.org/t/p/original';
 
   async function getDataFromApi() {
 
@@ -31,28 +37,27 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <div class="container">
-        <div class="row">
+      <div className="container">
+        <div className="row">
           {movies.map(item => (
-            <div class="col-sm-12 col-md-6 col-lg-12 d-flex py-3">
-              <div class="col-lg-2 position-relative image">
-                <img class="img-fluid" src={defaultPath + item.poster_path}/>
+            <div className="col-6 col-sm-6 col-md-4 col-lg-3 d-flex py-3">
+              <div className="col-lg-12 position-relative image">
+                <img className="img-fluid" src={defaultPath + item.poster_path}/>
                 { item.vote_average !== 0 && (
                   <>
-                    <span class="review-label d-flex justify-content-between align-items-center">
+                    <span className="review-label d-flex justify-content-between align-items-center">
                       <FontAwesomeIcon icon={faStar}/>
                       { item.vote_average.toFixed(1) }
                     </span>
                   </>
                 )}
               </div>
-              <div className="col-lg-10 movie-thumb">
+              <FullMoviePage title={item.title} overview={item.overview} release={item.release_date} />
+              {/* <div className="col-lg-10 movie-thumb">
                 <h3>{item.title}</h3>
                 <span class="d-block"><b>Sinopse</b>: {item.overview}</span>
                 <span class="d-block"><b>Data de Lançamento</b>: {item.release_date}</span>
-              </div>
+              </div> */}
             </div>
           ))}
         </div>
---
+++ src/components/FullMoviePage.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class FullMoviePage extends Component {
+    render() {
+        return (
+            <p>{this.props.title}</p>
+        );
+    }
+}
